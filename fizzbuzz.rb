15cd95e427b727a6a67961fb445921c3bdb6da8d
Sudheera Daggumalli <sudheeradaggumalli@gmail.com>
2017-09-06 07:28:53 +0000

    automatically backed up by learn

---
+++ fizzbuzz.rb
@@ -0,0 +1,10 @@
+def fizzbuz(value)
+    if value%3==0
+        puts "fizz"
+    elsif value%5==0
+        puts "buzz"
+    elsif value%15==0
+        puts "fizzbuzz"
+    else
+        puts " "
+end
