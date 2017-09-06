d9b13cb1006c9073e801091f12c402ca30ea962a
Sudheera Daggumalli <sudheeradaggumalli@gmail.com>
2017-09-06 07:47:25 +0000

    automatically backed up by learn

--- fizzbuzz.rb
+++ fizzbuzz.rb
@@ -1,10 +1,10 @@
 def fizzbuzz(value)
     #puts "#{value}"
-    if value%3==0
-          puts "Fizz"
-    elsif value%5==0
+    if value%3 == 0
+        puts "Fizz"
+    elsif value%5 == 0
           puts "Buzz"
-    elsif value%15==0
+    elsif value%15 == 0
           puts "FizzBuzz"
     else
           puts " "
