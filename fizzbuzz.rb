2fff99eb2fb3214a790e84d3693888cea1cdb1d6
Sudheera Daggumalli <sudheeradaggumalli@gmail.com>
2017-09-06 07:54:20 +0000

    automatically backed up by learn

--- fizzbuzz.rb
+++ fizzbuzz.rb
@@ -4,8 +4,8 @@ def fizzbuzz(value)
         "Fizz"
     elsif value%5 == 0
         "Buzz"
-    elsif value % 3 == 0 && value % 3 == 0
-        "FizzBuzz"
+    elsif value % 3 == 0 && value % 5 == 0
+         "FizzBuzz"
     else
         puts "nil"
     end
